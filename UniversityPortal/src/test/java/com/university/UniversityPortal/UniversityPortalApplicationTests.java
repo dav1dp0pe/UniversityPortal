f8bea579aff2649a07afe397a5e7368e5c1b39d3
dav1dp0pe <pope3705@gmail.com>
2025-12-26 13:19:19 -0500

    Initial Spring Boot Project

---
+++ UniversityPortal/src/test/java/com/university/UniversityPortal/UniversityPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.UniversityPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
